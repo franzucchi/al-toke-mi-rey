1af830f56125bffd27fd0cfaabfec46e4a6d2596
fran zucchi <franzucchi11@gmail.com>
2020-06-25 19:44:07 -0300

    agrega index.html

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ const port = process.env.PORT || 3000;
 // crea el objeto app
 const app = express();
 
+// agregamos esta línea
+app.use(express.static('public'));
+
 // la app responde con Hello world
 // a todas las peticiones GET a /
 app.get('/', (req, res) => {
@@ -16,4 +19,4 @@ app.get('/', (req, res) => {
 });
 
 // el server escucha en el puerto 3000
-app.listen(3000);
+app.listen(port);
